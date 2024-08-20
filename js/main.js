ff89a0d354f6f814878887e781debb91c9282778
saramahmoud20 <sara.m.desoukey@gmail.com>
2024-08-20 09:44:36 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,61 @@
+
+
+
+// document.getElementById("demo").innerHTML = "Hello from external js File";
+document.getElementById("demo").style.color = "rgb(255,255,255)";
+document.getElementById("demo").style.backgroundColor = "blue";
+document.getElementById("demo").style.textAlign = "center";
+
+
+
+// alert("hi")
+
+// console.log("hiiii")
+
+// var x = 10;
+// var y = 15;
+// var z = x+y
+// document.getElementById('demo').innerHTML=z;
+
+// document.write(z);
+
+
+
+//date type
+var number = 5; //number
+var h = 6.4; //number
+var z = "ahmed"; //string
+var g = "6"; //string
+var w = true; //boolen
+var a = false; //boolen
+
+console.log(typeof w);
+
+var names = ["Ahmed", "Mohamed", "Mahmoud", "Ali", true,5.2, 6, false];
+
+var person = {
+    FirstName : "Ahmed",
+    LasrName : "Mohamed",
+    Salary : 5000,
+}
+
+console.log(typeof person)
+
+console.log(typeof names)
+
+
+var k;
+
+console.log( k)
+
+
+var u = null;
+
+console.log(typeof u)
+
+
+
+
+
+
+
